a442c3b4cdb1f2f6c18130ef06f7119cffa6eb6b
AdityaVK2006 <adityakatepallewar@gmail.com>
2024-09-11 18:22:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+import { ARButton } from 'three/addons/webxr/ARButton.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+})
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+document.body.appendChild(ARButton.createButton(renderer));
+renderer.xr.enabled = true;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = -3;
+controls.update();
+
+const sunTex = new THREE.TextureLoader().load('textures/sun.jpeg');
+const sun = new THREE.Mesh(new THREE.SphereGeometry(1.5), new THREE.MeshBasicMaterial({map : sunTex}));
+scene.add(sun);
+
+const earthTex = new THREE.TextureLoader().load('textures/earth.jpg');
+const earth = new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshBasicMaterial({map : earthTex}));
+sun.add(earth);
+earth.position.x = 5
+
+const moonTex = new THREE.TextureLoader().load('textures/moon.jpg');
+const moon = new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({map : moonTex}));
+earth.add(moon);
+moon.position.x = 0.8
+
+renderer.setAnimationLoop(()=>{
+    sun.rotation.y += 0.005
+    earth.rotation.y += 0.01
+    renderer.render(scene, camera);
+})
